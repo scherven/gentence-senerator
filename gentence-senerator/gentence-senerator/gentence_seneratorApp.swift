9327b11860152e58b247ef927f980eec6ed84709
Simon Chervenak <sarras305@gmail.com>
2025-12-30 15:15:00 -0500

    senerate gentences

---
+++ gentence-senerator/gentence-senerator/gentence_seneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  gentence_seneratorApp.swift
+//  gentence-senerator
+//
+//  Created by Simon Chervenak on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct gentence_seneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gentence-senerator/gentence-seneratorTests/gentence_seneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  gentence_seneratorTests.swift
+//  gentence-seneratorTests
+//
+//  Created by Simon Chervenak on 12/30/25.
+//
+
+import Testing
+@testable import gentence_senerator
+
+struct gentence_seneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
